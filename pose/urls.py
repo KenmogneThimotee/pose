ce203681ee3e9d3fa49a0285322f978cf34dde64
Kenmogne Thimotee <kenmognethimotee@gmail.com>
2021-10-16 11:13:29 +0100

    work but not correct

--- pose/urls.py
+++ pose/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from .views import comparePose, setImageSource
 
 urlpatterns = [
     path("comparePose/", comparePose, name="compare"),
-    path("setsource/", setImageSource, name="setSource")
+    path("setsource/", setImageSource, name="set")
 ]
 
--- pose/views.py
+++ pose/views.py
@@ -7,21 +7,19 @@ import tensorflow_hub as hub
 import cv2
 from .forms import UploadFileForm
 from rest_framework.response import Response
-from django.conf import settings
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.conf import settings
 
 
+def comparator(image):
 
+    #image = tf.convert_to_tensor(image)
 
-
-
-def comparator(image_1):
-
-    image = tf.convert_to_tensor(image_1)
-
-    image = tf.compat.v1.image.decode_jpeg(image)
-    image = tf.compat.v1.image.decode_jpeg(image)
+    #image = tf.compat.v1.image.decode_jpeg(image)
+    #image = tf.compat.v1.image.decode_jpeg(image)
     image = tf.expand_dims(image, axis=0)
     # Resize and pad the image to keep the aspect ratio and fit the expected size.
     image = tf.cast(tf.image.resize_with_pad(image, 192, 192), dtype=tf.int32)
@@ -29,11 +27,6 @@ def comparator(image_1):
     # Download the model from TF Hub.
     model = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
     movenet = model.signatures['serving_default']
-
-    # Run model inference.
-    outputs = movenet(image)
-    # Output is a [1, 1, 17, 3] tensor.
-    keypoints = outputs['output_0']
 
     # Run model inference.
     outputs = movenet(image)
@@ -50,18 +43,28 @@ def normalize(input_test):
 		input_test[:,k] = input_test[:,k]/np.linalg.norm(input_test[:,k])	
 	return input_test
 
-def cosineSimilarity(self, inputs, source):
+def cosineSimilarity(inputs, source):
 
-		inputs = normalize(inputs)
-		source = normalize(source)
+        #inputs = normalize(inputs)
+        #source = normalize(source)
 
-		similarity = cosine_similarity(inputs, source) + 1
+        print("inputs")
+        print(inputs.shape)
 
-		similarity = similarity / 2
+        print("source")
+        print(source.shape)
 
-		return similarity * 100
+        similarity = cosine_similarity(inputs.reshape(1,-1), source.reshape(1,-1)) + 1
+
+        similarity = similarity / 2
+
+        print("Similarity test size")
+        print(similarity)
+
+        return similarity * 100
 
 
+@api_view(['POST'])
 def comparePose(request):
 
     if request.method == 'POST':
@@ -70,42 +73,49 @@ def comparePose(request):
 
             fl = request.FILES['file']
             if fl.size > 3000000:
-                return Response({"message":"Taille du fichier superieur a 3mb"})
+                return Response({"message":"Taille du fichier superieur a 3mb"},
+                status=status.HTTP_406_NOT_ACCEPTABLE)
             else:
                 img_str = fl.read()
                 nparr = np.fromstring(img_str, np.uint8)
-                img_np = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
+                img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
                 input_new_coords = comparator(img_np)
 
-                saved_profile = np.load(settings.PATH_SOURCE)
+                
+                try:
+                    saved_profile = np.load(settings.PATH_SOURCE)
+                except:
+                    return Response({"message":"Set source image first"}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
-                score = score = cosineSimilarity(input_new_coords, saved_profile)
+                score = cosineSimilarity(input_new_coords, saved_profile)
 
                 if score >= 70:
-                    return Response({"message":"same"})
+                    return Response({"message":"same"}, status=status.HTTP_200_OK)
                 else:
-                    return Response({"message":"diff"})
+                    return Response({"message":"diff"}, status=status.HTTP_200_OK)
         else:
-            return Response({"message":" unsuccess"})
+            return Response({"message":" unsuccess"}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
+
+@api_view(['POST'])
 def setImageSource(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             fl = request.FILES['file']
             if fl.size > 3000000:
-                return Response({"message":"Taille du fichier superieur a 3mb"})
+                return Response({"message":"Taille du fichier superieur a 3mb"}, status=status.HTTP_406_NOT_ACCEPTABLE)
             else:
                 img_str = fl.read()
                 nparr = np.fromstring(img_str, np.uint8)
-                img_np = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
+                img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
                 #prediction
                 keypoints = comparator(img_np)
                 np.save(settings.PATH_SOURCE, keypoints)
 
-            return Response({"message":"success"})
+            return Response({"message":"success"}, status=status.HTTP_200_OK)
         else:
-            return Response({"message":" unsuccess"})
+            return Response({"message":" unsuccess"}, status=status.HTTP_200_OK)
 
 
 
