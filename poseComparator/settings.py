cb6424b368f8bd077fb0b49d194d8f642e9a63e2
Kenmogne Thimotee <44063958+KenmogneThimotee@users.noreply.github.com>
2021-12-03 09:30:41 +0000

    Update settings.py

--- poseComparator/settings.py
+++ poseComparator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u8@w0$1z&iv5+x*0rlk7b_!*db#b58d5se+u((-(3a)qkg8j5m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
